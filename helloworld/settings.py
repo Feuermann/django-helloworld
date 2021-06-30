9bb69172f52465f7f63a97d30804dbd99c7903fe
p.suhon <p.suhon@naveksoft.com>
2021-06-30 12:24:46 +0300

    fix to Mysql db

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'helloworld.db'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "django_hello",
+        'USER': "django",
+        'PASSWORD': 'django_password',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
